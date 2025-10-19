c9d30a481a0c17a0b0c94b5732e1f8c83ec34668
alzain1912 <zainxx.1904@gmail.com>
2025-10-19 13:55:45 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getelementbyid("contracform").onsubmit = function () {alert ("yiur inquiry has been submitted");
+ return false:
+   };
+
+document.getelementsbytags("tr")[1].onclick =function () {
+ alert ("candles are sold out"):
